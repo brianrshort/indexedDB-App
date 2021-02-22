0d47427e588745e01975a5f9507427992d61a219
Brian Short <brian.ralph.short@gmail.com>
2021-02-22 17:38:27 -0500

    Add asynchronous autoincrementing create object store function

--- public/db.js
+++ public/db.js
@@ -2,3 +2,16 @@ import { openDB } from 'https://unpkg.com/idb?module';
 
 let db;
 
+( async ()=> {
+    db = await openDB("budget", 1, {
+      upgrade(db) {
+        const objectStore = db.createObjectStore("pending", {
+          keyPath: "offlineId",
+          autoIncrement: true 
+          });
+  
+        console.log( `~ created the db/upgraded it:`, objectStore.name );
+      } });
+  })();
+  
+  
